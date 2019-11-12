facba103e9f858abbf36963ca7b2aa37295a90c5
dekaqqq <kdrushlyak@gmail.com>
2019-11-12 15:46:15 +0200

    Updates

---
+++ precache-manifest.662436914f268445cd0e044999e37470.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7af529bb2e42431f031d21eb635c4dce",
+    "url": "/product_list/index.html"
+  },
+  {
+    "revision": "1f5d186fb438e0894be2",
+    "url": "/product_list/static/css/2.eb4edab8.chunk.css"
+  },
+  {
+    "revision": "d7a32c56e73577304b44",
+    "url": "/product_list/static/css/main.cd4f00ff.chunk.css"
+  },
+  {
+    "revision": "1f5d186fb438e0894be2",
+    "url": "/product_list/static/js/2.d92b1fb8.chunk.js"
+  },
+  {
+    "revision": "d7a32c56e73577304b44",
+    "url": "/product_list/static/js/main.739e8d2a.chunk.js"
+  },
+  {
+    "revision": "97099d735d47eccda1cd",
+    "url": "/product_list/static/js/runtime~main.fd6b12aa.js"
+  }
+]);
